313f4184ae89adeac54ca42469e30be60cf03bf3
crubio01 <crubio01@gmail.com>
2018-08-02 20:24:46 -0500

    index and appmodel comments updates

--- test/unit/server/applicationModel.spec.js
+++ test/unit/server/applicationModel.spec.js
@@ -1,10 +1,5 @@
 /********************************************************** {COPYRIGHT-TOP} ****
- * Licensed Materials - Property of IBM
- *
- * (C) Copyright IBM Corp. 2014 All Rights Reserved
- *
- * US Government Users Restricted Rights - Use, duplication, or
- * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ * 
  ********************************************************* {COPYRIGHT-END} ****/
 /*jshint expr: true*/
 
